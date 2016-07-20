0f1012e1d0d88ee125f31e4e2b879fe5fdacd899
Alexander Portnoy <aportnoy@pushwoosh.com>
2016-07-20 17:25:45 +0600

    Added extra-headers needed by Grafana

--- index.js
+++ index.js
@@ -49,11 +49,30 @@ for (var i = 0;i < annotations.length; i++) {
 }
 
 app.all('/', function(req, res) {
+  var headers = {};
+  headers["Access-Control-Allow-Origin"] = "*";
+  headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+  headers["Access-Control-Allow-Credentials"] = false;
+  headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+  headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+
+  res.writeHead(200, headers);
+
   res.send('I have a quest for you!');
   res.end();
 });
 
 app.all('/search', function(req, res){
+  var headers = {};
+  headers["Access-Control-Allow-Origin"] = "*";
+  headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+  headers["Access-Control-Allow-Credentials"] = false;
+  headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+  headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+  headers["Content-Type"] = "application/json; charset=utf-8";
+
+  res.writeHead(200, headers);
+
   var result = [];
   _.each(timeserie, function(ts) {
     result.push(ts.target);
@@ -64,6 +83,16 @@ app.all('/search', function(req, res){
 });
 
 app.all('/annotations', function(req, res) {
+  var headers = {};
+  headers["Access-Control-Allow-Origin"] = "*";
+  headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+  headers["Access-Control-Allow-Credentials"] = false;
+  headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+  headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+  headers["Content-Type"] = "application/json; charset=utf-8";
+
+  res.writeHead(200, headers);
+
   console.log(req.url);
   console.log(req.body);
 
@@ -72,6 +101,16 @@ app.all('/annotations', function(req, res) {
 })
 
 app.all('/query', function(req, res){
+  var headers = {};
+  headers["Access-Control-Allow-Origin"] = "*";
+  headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+  headers["Access-Control-Allow-Credentials"] = false;
+  headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+  headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+  headers["Content-Type"] = "application/json; charset=utf-8";
+
+  res.writeHead(200, headers);
+
   console.log(req.url);
   console.log(req.body);
 
